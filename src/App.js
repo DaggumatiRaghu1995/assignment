5bda14a484c89fb65297f229da481bae8d8aeda5
DaggumatiRaghu1995 <120002124+DaggumatiRaghu1995@users.noreply.github.com>
2022-12-07 14:52:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import LineChart from "./Components/LineChart";
 import Leafletmap from "./Components/Leafletmap";
 import "./App.css";
-import { useState } from "react";
 function App() {
   return (
     <div className="App">
